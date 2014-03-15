259c90536282ff5b42eddb7c0b765c86d079d247
Daniel Stankevich <daniel@air>
2014-03-16 01:58:09 +1100

    ui version 2 poc

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 $( document ).ready(function() {
   $('#tooltip').tooltip();
+
+  // Hide button
+  $('.sidebar-toggle').on('click', function() {
+    $('.mutio-sidebar').toggle();
+    $('.mutio-preview').toggleClass('col-md-9').toggleClass('col-md-12');
+  })
+
 });
