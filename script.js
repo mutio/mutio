b7c9317151bf2a80855f127dd2aa36b5dd741ea6
Daniel Stankevich <daniel@air>
2014-03-16 21:04:58 +1100

    reset button, default config in textarea

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
-$( document ).ready(function() {
+$(document).ready(function() {
+  init();
+});
+
+function init() {
+  // Tooltips
   $('#tooltip').tooltip();
 
-  // Hide button
+  // Sidebar toggle
   $('.sidebar-toggle').on('click', function() {
     $('.mutio-sidebar').toggle();
     $('.mutio-preview').toggleClass('col-md-9').toggleClass('col-md-12');
   })
 
-});
+  // Reset button
+  $('.conf-reset').on('click', function() {
+    $('#config').val(null);
+  })
+
+}
